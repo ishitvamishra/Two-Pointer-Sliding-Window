faca1866bd9259ead411586cee15acb1ba2268b6
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-01-23 19:03:27 +0530

    Time: 3 ms (28.59%) | Memory: 45.3 MB (46.86%) - LeetSync

---
+++ 174-dungeon-game/dungeon-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int n = dungeon.length;
+        int m = dungeon[0].length;
+
+        for(int i = n-1;i>=0;i--){
+            for(int j = m-1;j>=0;j--){
+                if(i == n-1 && j == m-1){
+                    dungeon[i][j] =Math.max(1, 1 - dungeon[i][j]); 
+                }
+                if(i == n-1 && j< m-1){
+                    dungeon[i][j] = Math.max(1, dungeon[i][j+1] - dungeon[i][j]); 
+                }
+
+                if(i < n-1 && j == m-1){
+                    dungeon[i][j] = Math.max(1, dungeon[i+1][j] - dungeon[i][j]);
+                }
+
+                if(i < n-1 && j < m-1){
+                    dungeon[i][j] = Math.max(1, Math.min(dungeon[i][j+1], dungeon[i+1][j])- dungeon[i][j]);
+                }
+            }
+        }
+        return dungeon[0][0];
+    }
+}
