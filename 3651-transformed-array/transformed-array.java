ab185e38d6e389fd4d1b8c29fd303e67887ec173
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-02-05 19:21:47 +0530

    Time: 1 ms (98.72%) | Memory: 46.9 MB (30.77%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -1,18 +1,13 @@
 class Solution {
     public int[] constructTransformedArray(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[nums.length];
-        for(int i =0;i<nums.length;i++){
-            if(nums[i] > 0){
-                arr[i] = nums[(nums[i]+i)%n];
-            }
-            else if(nums[i] < 0){
-                arr[i] = nums[(n+(i-Math.abs(nums[i]))%n)%n];
-            }
-            else{
-                arr[i] = nums[i];
-            }
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int idx = ((i + nums[i]) % n + n) % n;
+            arr[i] = nums[idx];
         }
+
         return arr;
     }
 }
