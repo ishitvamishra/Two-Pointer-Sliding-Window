b88910006360010071d5b03ef94640e5da4006b9
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2026-02-06 21:14:38 +0530

    Time: 376 ms (30.65%) | Memory: 43 MB (28.08%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean solveSudoku(char[][] board) {
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                
+                if(board[i][j] == '.'){
+                    
+                    for(char c = '1'; c <= '9'; c++){
+                        
+                        if(isSafe(board, i, j, c)){
+                            board[i][j] = c;
+                            
+                            if(solveSudoku(board))
+                                return true;
+                            
+                            board[i][j] = '.'; // backtrack
+                        }
+                    }
+                    
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isSafe(char[][] board, int row, int col, char num){
+        
+        for(int i = 0; i < 9; i++){
+            if(board[row][i] == num) return false;
+            if(board[i][col] == num) return false;
+            
+            int r = 3*(row/3) + i/3;
+            int c = 3*(col/3) + i%3;
+            if(board[r][c] == num) return false;
+        }
+        
+        return true;
+    }
+}
